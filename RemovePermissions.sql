6ab54393727765b480b340ce615451c6191814f8
Heiner <heinermongelopez@gmail.com>
2022-05-26 12:18:53 -0600

    Refactor

--- RemovePermissions.sql
+++ RemovePermissions.sql
@@ -1,35 +1,43 @@
-USE AdventureWorks2019
+USE TRANSACTION_PROCESSING_EXAMEN
 GO
-CREATE PROCEDURE sp_RemovePermissions
-@param_Name VARCHAR(50) = NULL 
-,@param_Table_Name VARCHAR(50) = NULL 
-,@param_Permission_Read INT = NULL
-,@param_Permission_Write INT = NULL
-,@param_Permission_AddUsers INT = NULL
+
+
+CREATE OR ALTER PROCEDURE sp_RemovePermissions
+	@param_Name VARCHAR(50) = NULL 
+	,@param_Table_Name VARCHAR(50) = NULL 
+	,@param_Permission INT = NULL
 AS
-BEGIN
-PRINT 'PROBANDO RemovePermissions'
+	BEGIN
+		DECLARE @r nvarchar(4000)
+		IF(@param_Permission LIKE 1)
+			BEGIN
+				BEGIN TRY
+					BEGIN TRANSACTION
+				--Crear logins
+					SET @r = N'REVOKE EXEC ON sp_CreateLogin TO ' + QUOTENAME(@param_Name,'[]') 
+					EXEC(@r)
+				--Agregar usuarios
+					SET @r = N'REVOKE EXEC ON sp_AddUsers TO ' + QUOTENAME(@param_Name,'[]') 
+					EXEC(@r)
+					COMMIT
+					PRINT ('Permiso para crear usuarios revocado')
+				END TRY
+				BEGIN CATCH
+					PRINT ('ERROR: No se ha podido eliminar el permiso para crear usuarios')
+					ROLLBACK
+				END CATCH
+			END
+		IF(@param_Permission LIKE 2)
+			BEGIN
+				BEGIN TRY
+					SET @r = N'REVOKE SELECT ON ' + @param_Table_Name + ' TO ' + QUOTENAME(@param_Name,'[]') 
+					EXEC(@r)
+					PRINT ('Permiso para leer revocado en la tabla: ' + @param_Table_Name)
+				END TRY
+				BEGIN CATCH
+					PRINT ('ERROR: No se ha podido eliminar el permiso')
+				END CATCH
+			END
+	END
 
-IF(@param_Permission_Read LIKE 1)
-BEGIN
-PRINT 'Permiso leer'
-
-DECLARE @r nvarchar(4000)
-			SET @r = N'DENY SELECT ON ' + @param_Table_Name + ' TO ' + QUOTENAME(@param_Name,'[]') 
-		EXEC(@r)
-END
-IF(@param_Permission_Write LIKE 2)
-BEGIN
-PRINT 'Permiso escribir'
-DECLARE @w nvarchar(4000)
-			SET @w = N'DENY INSERT ON ' + @param_Table_Name  + ' TO ' + QUOTENAME(@param_Name,'[]') 
-		EXEC(@w)
-END
-IF(@param_Permission_AddUsers LIKE 3)
-BEGIN
-PRINT 'Permiso usuarios'
-END
-
-	PRINT 'LISTO RemovePermissions'
-
-END
+EXEC sp_RemovePermissions 'Heiner', 'CLI_COMMON.tb_CLIENTS', 2
