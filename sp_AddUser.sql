6d2b84e38467ff2c47cc1d624d477a864c9721f1
Heiner <heinermongelopez@gmail.com>
2022-05-26 11:50:20 -0600

    Agrega Try-Catch, modifica el nombre del procedimiento y cambia la base de datos a usar.

--- sp_AddUser.sql
+++ sp_AddUser.sql
@@ -1,20 +1,26 @@
-
 --ADD LOGIN
-CREATE OR ALTER PROCEDURE sp_AddUsers
+USE TRANSACTION_PROCESSING_EXAMEN
+GO
+
+CREATE OR ALTER PROCEDURE sp_CreateLogin
 	@param_Name VARCHAR(50) = NULL 
 	,@param_Password VARCHAR(50) = NULL 
 AS
 	BEGIN
-		PRINT 'PROBANDO addLogin'
 		--Se crea un login
 			DECLARE @t nvarchar(4000)
-				SET @t = N'CREATE LOGIN ' + QUOTENAME(@param_Name,'[]') + 'WITH PASSWORD=' + QUOTENAME(@param_Password,'''') + ', DEFAULT_DATABASE=' + QUOTENAME('TRANSACTION_PROCESSING_EXAMEN','[]')
+		BEGIN TRY
+			SET @t = N'CREATE LOGIN ' + QUOTENAME(@param_Name,'[]') + 'WITH PASSWORD=' + QUOTENAME(@param_Password,'''') + ', DEFAULT_DATABASE=' + QUOTENAME('TRANSACTION_PROCESSING_EXAMEN','[]')
 			EXEC(@t)
-		PRINT 'LISTO addLogin'
+			PRINT 'Login creado'
+		END TRY
+		BEGIN CATCH
+			PRINT 'No se ha podido crear el login'
+		END CATCH
 	END
 GO
 
---ADD USER
+--ADD USER TO DATABASE
 USE TRANSACTION_PROCESSING_EXAMEN
 GO
 
@@ -22,11 +28,17 @@ CREATE OR ALTER PROCEDURE sp_AddUsers
 @param_Name VARCHAR(50) = NULL 
 AS
 	BEGIN
-		PRINT 'PROBANDO addUser'
 		DECLARE @t nvarchar(4000)
-		SET @t = N'CREATE USER ' + QUOTENAME(@param_Name,'[]') + 'FOR LOGIN ' + QUOTENAME(@param_Name,'[]') 
-		EXEC(@t)
-		PRINT 'LISTO addUser'
+		BEGIN TRY
+			SET @t = N'CREATE USER ' + QUOTENAME(@param_Name,'[]') + 'FOR LOGIN ' + QUOTENAME(@param_Name,'[]') 
+			EXEC(@t)
+			PRINT 'Usuario agregado a la base de datos TRANSACTION_PROCESSING_EXAMEN'
+		END TRY
+		BEGIN CATCH
+			PRINT 'Usuario agregado a la base de datos TRANSACTION_PROCESSING_EXAMEN'
+		END CATCH
 	END
 GO
 
+--EXEC sp_CreateLogin 'Heiner', '12345'
+--EXEC sp_AddUsers 'Heiner'
