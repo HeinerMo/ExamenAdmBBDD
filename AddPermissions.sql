faf458f74b49291e350be036f3a46322eff70680
Heiner <heinermongelopez@gmail.com>
2022-05-26 12:01:13 -0600

    Se corrige posible error de concurrencia[F

--- AddPermissions.sql
+++ AddPermissions.sql
@@ -8,23 +8,27 @@ CREATE OR ALTER PROCEDURE sp_AddPermissions
 	,@param_Permission INT = NULL
 AS
 	BEGIN
-		PRINT 'PROBANDO AddPermissions'
 		DECLARE @r nvarchar(4000)
 		IF(@param_Permission LIKE 1)
 			BEGIN
-				PRINT 'Permiso usuarios'
 				BEGIN TRY
-					SET @r = N'GRANT EXEC ON sp_AddPermissions TO ' + QUOTENAME(@param_Name,'[]') 
+					BEGIN TRANSACTION
+				--Crear logins
+					SET @r = N'GRANT EXEC ON sp_CreateLogin TO ' + QUOTENAME(@param_Name,'[]') 
 					EXEC(@r)
+				--Agregar usuarios
+					SET @r = N'GRANT EXEC ON sp_AddUsers TO ' + QUOTENAME(@param_Name,'[]') 
+					EXEC(@r)
+					COMMIT
 					PRINT ('Permiso para crear usuarios asignado')
 				END TRY
 				BEGIN CATCH
-					PRINT ('ERROR: No se ha podido asignar el permiso')
+					PRINT ('ERROR: No se ha podido asignar el permiso para crear usuarios')
+					ROLLBACK
 				END CATCH
 			END
 		IF(@param_Permission LIKE 2)
 			BEGIN
-				PRINT 'Permiso leer'
 				BEGIN TRY
 					SET @r = N'GRANT SELECT ON ' + @param_Table_Name + ' TO ' + QUOTENAME(@param_Name,'[]') 
 					EXEC(@r)
@@ -33,8 +37,5 @@ AS
 				BEGIN CATCH
 					PRINT ('ERROR: No se ha podido asignar el permiso')
 				END CATCH
-				
 			END
 	END
-
-EXEC sp_AddPermissions'Heiner', 'tabla', 1
