8db25011bbfc34a2084ec882dcc016e3e0237e70
Heiner <heinermongelopez@gmail.com>
2022-05-28 12:09:44 -0600

    Ser corrigen errores sobre la selección de tablas

--- RemovePermissions.sql
+++ RemovePermissions.sql
@@ -15,7 +15,10 @@ AS
 					BEGIN TRANSACTION
 				--Crear logins
 					SET @r = N'REVOKE EXEC ON sp_CreateLogin TO ' + QUOTENAME(@param_Name,'[]') 
-					EXEC(@r)
+					EXEC(@r)						--actualizar tabla usuarios
+						UPDATE CLI_COMMON.tb_USERS
+						SET [can_create_users] = 0
+						WHERE user_name = @param_Name
 					COMMIT
 					PRINT ('Permiso para crear usuarios revocado')
 				END TRY
@@ -28,7 +31,18 @@ AS
 			BEGIN
 				BEGIN TRY
 					SET @r = N'REVOKE SELECT ON ' + @param_Table_Name + ' TO ' + QUOTENAME(@param_Name,'[]') 
-					EXEC(@r)
+					EXEC(@r)						
+					--actualizar tabla usuarios
+					UPDATE CLI_COMMON.tb_USERS
+					SET [can_read] = 0
+					WHERE user_name = @param_Name
+
+					IF EXISTS(SELECT TOP 1 1 FROM CLI_COMMON.tb_USERS_TABLES WHERE table_name = @param_Table_Name AND user_name = @param_Name)
+						BEGIN
+							DELETE CLI_COMMON.tb_USERS_TABLES
+							WHERE user_name = @param_Name AND table_name = @param_Table_Name
+						END
+
 					PRINT ('Permiso para leer revocado en la tabla: ' + @param_Table_Name)
 				END TRY
 				BEGIN CATCH
@@ -37,4 +51,4 @@ AS
 			END
 	END
 
-EXEC sp_RemovePermissions 'Heiner', 'CLI_COMMON.tb_CLIENTS', 2
+--EXEC sp_RemovePermissions 'Heiner', '[CUSTOMERS].[tb_CUSTOMER_ACCOUNTS]', 2
